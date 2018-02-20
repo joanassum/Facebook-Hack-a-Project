6773d6ffab8257b0676264bf04ede501e35f85b5
Joan <joan@example.com>
2018-02-20 19:20:05 +0000

    add menu to friends activity

--- Meep-upScheduleApp/app/src/main/java/com/example/joan/meep_upscheduleapp/FriendsActivity.java
+++ Meep-upScheduleApp/app/src/main/java/com/example/joan/meep_upscheduleapp/FriendsActivity.java
@@ -2,8 +2,19 @@ package com.example.joan.meep_upscheduleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class FriendsActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.friends_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
