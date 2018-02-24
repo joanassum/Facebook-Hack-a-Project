249388cb9a7e2e1a545327df256ed15ff4342de4
Joan <joan@example.com>
2018-02-24 05:28:12 +0000

    add item click function on menu in FriendsActivity and friendsListView id

--- Meep-upScheduleApp/app/src/main/java/com/example/joan/meep_upscheduleapp/FriendsActivity.java
+++ Meep-upScheduleApp/app/src/main/java/com/example/joan/meep_upscheduleapp/FriendsActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class FriendsActivity extends AppCompatActivity {
 
@@ -14,6 +15,17 @@ public class FriendsActivity extends AppCompatActivity {
         menuInflater.inflate(R.menu.friends_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.addNewFriends):
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
